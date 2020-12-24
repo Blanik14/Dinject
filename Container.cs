2f90e612aeab9c1a00dc12047674ae6333853229
Nils Macke <mail@nilsmacke.de>
2020-12-24 02:25:48 +0100

    First Draft of Container Class

--- Container.cs
+++ Container.cs
@@ -1,10 +1,120 @@
-﻿using System;
+﻿using Dinject.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Dinject
 {
-	class Container
+	public class Container
 	{
+		private IDictionary<Type, ImplementationType> _registeredTypes;
+
+		private IDictionary<Type, ImplementationType> _registeredDependencyTypes;
+
+		public Container()
+		{
+			this._registeredTypes = new Dictionary<Type, ImplementationType>();
+			this._registeredDependencyTypes = new Dictionary<Type, ImplementationType>();
+		}
+
+		public void Register<TInterface, TImplementation>() where TImplementation : class
+		{
+			var interfaceType = typeof(TInterface);
+			var implementationType = typeof(TImplementation);
+
+			if (this.IsRegistered(interfaceType) || this.IsRegistered(implementationType))
+			{
+				throw new Exception("Type already Registered Exception");
+			}
+
+			this._registeredDependencyTypes.Add(interfaceType, new ImplementationType(implementationType));
+			this._registeredTypes.Add(implementationType, this._registeredDependencyTypes[interfaceType]);
+		}
+
+		public void Register<TImplementation>() where TImplementation : class
+		{
+			var implementationType = typeof(TImplementation);
+
+			if (this.IsRegistered(implementationType))
+			{
+				throw new Exception("Type already Registered Exception");
+			}
+
+			this._registeredTypes.Add(implementationType, new ImplementationType(implementationType));
+		}
+
+		public TRegistered GetInstance<TRegistered>()
+		{
+			return (TRegistered)this.GetInstance(typeof(TRegistered));
+		}
+
+		public object GetInstance(Type instanceType)
+		{
+			if (!this.IsRegistered(instanceType))
+			{
+				throw new Exception("TypeNotRegistered Exception");
+			}
+
+			ImplementationType implType = this.GetRegisteredTypeFromType(instanceType);
+			Type[] parameters = this.GetParametersFromType(implType);
+
+			object[] parameterInstances = new object[parameters.Length];
+
+			for (int i = 0; i < parameters.Length; i++)
+			{
+				parameterInstances[i] = this.GetInstance(parameters[i]);
+			}
+
+			return Activator.CreateInstance(instanceType, parameterInstances);
+		}
+
+		private Type[] GetParametersFromType(ImplementationType implType)
+		{
+			Type[] parameters = new Type[implType.Parameters.Count];
+			for (int i = 0; i < parameters.Length; i++)
+			{
+				var parameterType = implType.Parameters[i].ParameterType;
+				if (this.IsRegistered(parameterType))
+				{
+					parameters[i] = parameterType;
+				}
+			}
+
+			return parameters;
+		}
+
+		private ImplementationType GetRegisteredTypeFromType(Type instanceType)
+		{
+			ImplementationType implType = null;
+
+			if (this._registeredDependencyTypes.ContainsKey(instanceType))
+			{
+				implType = this._registeredDependencyTypes[instanceType];
+			}
+			else if (this._registeredTypes.ContainsKey(instanceType))
+			{
+				implType = this._registeredTypes[instanceType];
+			}
+
+			return implType;
+		}
+
+		public bool IsRegistered(Type type)
+		{
+			if (this._registeredDependencyTypes.ContainsKey(type))
+			{
+				return true;
+			}
+			if (this._registeredTypes.ContainsKey(type))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool IsRegistered<T>()
+		{
+			return this.IsRegistered(typeof(T));
+		}
 	}
 }
---
+++ Core/ImplementationType.cs
@@ -0,0 +1,72 @@
+﻿using Dinject.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+namespace Dinject.Core
+{
+	public class ImplementationType
+	{
+		public Type Type { get; private set; }
+
+		public IList<ParameterInfo> Parameters { get; private set; }
+
+		public ImplementationType(Type type)
+		{
+			this.Type = type;
+			this.Initialize(this.Type);
+		}
+
+		private void Initialize(Type type)
+		{
+			this.Parameters = this.GetParameters(this.GetDIConstructor(type));
+		}
+
+		private ConstructorInfo GetDIConstructor(Type type)
+		{
+			ConstructorInfo usedConstructor = null;
+			var constructors = type.GetConstructors();
+
+			if (constructors.Length > 1)
+			{
+				foreach (var constructor in constructors)
+				{
+					var attribute = constructor.GetCustomAttributes<DIConstructorAttribute>();
+					if (attribute != default(DIConstructorAttribute))
+					{
+						usedConstructor = constructor;
+					}
+				}
+			}
+			else
+			{
+				usedConstructor = constructors[0];
+			}
+
+			if (usedConstructor == null)
+			{
+				throw new Exception("DIConstructor not found");
+			}
+
+			return usedConstructor;
+		}
+
+		private IList<ParameterInfo> GetParameters(ConstructorInfo constructorInfo)
+		{
+			var parameters = constructorInfo.GetParameters();
+			return parameters.OrderBy(p => p.Position).ToList();
+		}
+	}
+
+	public class ImplementationType<T> : ImplementationType where T : class
+	{
+		public ImplementationType() 
+			:base(typeof(T))
+		{
+		}
+
+		
+	}
+}
